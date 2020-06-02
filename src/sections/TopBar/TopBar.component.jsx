5ed5687a74251658021b39e8b8c2cadc2cb7c77f
Ben Terry <beterry12@gmail.com>
2020-06-02 15:05:38 -0400

    style TopBar

--- src/sections/TopBar/TopBar.component.jsx
+++ src/sections/TopBar/TopBar.component.jsx
@@ -3,11 +3,36 @@ import React, { Component } from 'react'
 //import styles
 import styles from './TopBar.module.scss'
 
+//import icons
+import {MdMenu, MdFilterList, MdStore} from 'react-icons/md'
+
 export default class TopBar extends Component {
     render() {
         return (
             <nav className={styles.topBar}>
-                <h1>Contracts</h1>
+                <div>
+                    <button>
+                        <MdMenu 
+                            color='#fff'
+                            size='1.5rem'
+                        />
+                    </button>
+                    <h1>Contracts</h1>
+                </div>
+                <div>
+                    <button>
+                        <MdFilterList 
+                            color='#fff'
+                            size='1.5rem'
+                        />
+                    </button>
+                    <button>
+                        <MdStore 
+                            color='#fff'
+                            size='1.5rem'
+                        />
+                    </button>
+                </div>
             </nav>
         )
     }
