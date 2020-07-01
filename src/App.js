b1fa593ff53c06addd90c063cff1d1e957b798ca
Ben Terry <beterry12@gmail.com>
2020-07-01 15:19:45 -0400

    refactor contract list to hold state in App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './styles/global.scss'
 
 import {
@@ -18,24 +18,82 @@ import Layout from './layout/Layout.component'
 import ContractList from './sections/ContractList/ContractList.component'
 import Details from './sections/Details/Details.component'
 
+//import utility function
+import sortContracts from './utility/sortContracts'
+
 //combine data to pass to children components
 const data = {...data1, ...data2}
 
-function App() {
-  return (
-    <Router>
-      <Layout>
-        <Switch>
-          <Route path="/contracts/:contractId">
-            <Details contracts={data}/>
-          </Route>
-          <Route path="/contracts">
-            <ContractList contracts={data}/>
-          </Route>
-        </Switch>
-      </Layout>
-    </Router>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+        allContracts: Object.values(data),
+        showCompleted: false,
+        sort: 'dateDesc'
+    }
+    this.toggleCompleted = this.toggleCompleted.bind(this)
+    this.changeSort = this.changeSort.bind(this)
+  }
+
+  toggleCompleted() {
+      this.setState((state, props) => ({showCompleted: !state.showCompleted}))
+  }
+
+  changeSort(newSort, e) {
+      if(this.state.sort === newSort) {
+          switch(newSort) {
+              case 'dateDesc':
+                  this.setState({sort: 'dateAsc'})
+                  break
+              case 'quantityDesc':
+                  this.setState({sort: 'quantityAsc'})
+                  break
+              case 'nameDesc':
+                  this.setState({sort: 'nameAsc'})
+                  break
+              case 'busDesc':
+                  this.setState({sort: 'busAsc'})
+                  break
+              default:
+                  this.setState({sort: 'dateDesc'})
+                  break
+          }
+          return
+      }
+      this.setState({sort: newSort})
+  }
+
+  render(){
+    let sortedContracts = this.state.allContracts
+    //filter out completed
+    if (!this.state.showCompleted) {
+        sortedContracts = sortedContracts.filter((contract) => contract.status !== 2)
+    }
+    // function to sort array based on state
+    sortedContracts = sortContracts(this.state.sort, sortedContracts)
+
+    return (
+      <Router>
+        <Layout>
+          <Switch>
+            <Route path="/contracts/:contractId">
+              <Details contracts={data}/>
+            </Route>
+            <Route path="/contracts">
+              <ContractList
+                sortedContracts={sortedContracts}
+                changeSort={this.changeSort}
+                toggleCompleted={this.toggleCompleted}
+                sort={this.state.sort}
+                showCompleted={this.state.showCompleted}
+              />
+            </Route>
+          </Switch>
+        </Layout>
+      </Router>
+    )
+  }
 }
 
 export default App;
--- src/sections/ContractList/ContractList.component.jsx
+++ src/sections/ContractList/ContractList.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom'
 
 //import styles
@@ -13,9 +13,6 @@ import Status from '../../components/Status/Status.component'
 
 //import icons
 import {MdChevronRight, MdArrowDownward} from 'react-icons/md'
-
-//import utility fubction
-import sortContracts from '../../utility/sortContracts'
 
 function Contract({name, business, quantity, starting, status, id}) {
     return (
@@ -36,135 +33,85 @@ function Contract({name, business, quantity, starting, status, id}) {
     )
 }
 
-export default class ContractList extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            allContracts: Object.values(this.props.contracts),
-            showCompleted: false,
-            sort: 'dateDesc'
-        }
-        this.toggleCompleted = this.toggleCompleted.bind(this)
-    }
-
-    toggleCompleted() {
-        this.setState((state, props) => ({showCompleted: !state.showCompleted}))
-    }
-
-    changeSort(newSort, e) {
-        if(this.state.sort === newSort) {
-            switch(newSort) {
-                case 'dateDesc':
-                    this.setState({sort: 'dateAsc'})
-                    break
-                case 'quantityDesc':
-                    this.setState({sort: 'quantityAsc'})
-                    break
-                case 'nameDesc':
-                    this.setState({sort: 'nameAsc'})
-                    break
-                case 'busDesc':
-                    this.setState({sort: 'busAsc'})
-                    break
-                default:
-                    this.setState({sort: 'dateDesc'})
-                    break
-            }
-            return
-        }
-        this.setState({sort: newSort})
-    }
-   
-    render() {
-        let sortedContracts = this.state.allContracts
-        //filter out completed
-        if (!this.state.showCompleted) {
-            sortedContracts = sortedContracts.filter((contract) => contract.status !== 2)
-        }
-        // function to sort array based on state
-        sortedContracts = sortContracts(this.state.sort, sortedContracts)
-        return (
-            <section className={styles.contractList}>
-                <ul>
-                    <li className={styles.header}>
-                        <div className={styles.left}>
-                            <button
-                                className={`${styles.col__name} ${styles.th}`}
-                                onClick={(e) => this.changeSort('nameDesc', e)}
-                            >
-                                <h6>name</h6>
-                                {
-                                    /* conditionally render arrow */
-                                    this.state.sort === 'nameDesc' ||
-                                    this.state.sort === 'nameAsc'
-                                    ? <MdArrowDownward size='1rem' className={`${styles.sortArrow} ${this.state.sort === 'nameAsc' ? 'flipped' : null}`}/>
-                                    : null
-                                }
-                            </button>
-                            
-                            <button
-                                className={`${styles.col__starting} ${styles.th}`}
-                                onClick={(e) => this.changeSort('dateDesc', e)}
-                            >
-                                <h6>starting</h6>
-                                {
-                                    /* conditionally render arrow */
-                                    this.state.sort === 'dateDesc' ||
-                                    this.state.sort === 'dateAsc'
-                                    ? <MdArrowDownward size='1rem' className={`${styles.sortArrow} ${this.state.sort === 'dateAsc' ? 'flipped' : null}`}/>
-                                    : null
-                                }
-                            </button>
-
-                            <button
-                                className={`${styles.col__business} ${styles.th}`}
-                                onClick={(e) => this.changeSort('busDesc', e)}
-                            >
-                                <h6>business</h6>
-                                {
-                                    /* conditionally render arrow */
-                                    this.state.sort === 'busDesc' ||
-                                    this.state.sort === 'busAsc'
-                                    ? <MdArrowDownward size='1rem' className={`${styles.sortArrow} ${this.state.sort === 'busAsc' ? 'flipped' : null}`}/>
-                                    : null
-                                }
-                            </button>
-
-                            <button
-                                className={`${styles.col__quantity} ${styles.th}`}
-                                onClick={(e) => this.changeSort('quantityDesc', e)}
-                            >
-                                {
-                                    /* conditionally render arrow */
-                                    this.state.sort === 'quantityDesc' ||
-                                    this.state.sort === 'quantityAsc'
-                                    ? <MdArrowDownward size='1rem' className={`${styles.sortArrow} ${this.state.sort === 'quantityAsc' ? 'flipped' : null}`}/>
-                                    : null
-                                }
-                                <h6>quantity</h6>
-                                
-                            </button>
-                        </div>
-                    </li>
-                    {sortedContracts.map(contract => 
-                        <Contract 
-                            name={contract.name}
-                            business={contract.business}
-                            quantity={numeral(contract.quantity).format("0,0")}
-                            starting={moment(contract.starting).format("MM/DD/YYYY")}
-                            status={contract.status}
-                            key={contract.id}
-                            id={contract.id}
-                        />
-                    )}
-                </ul>
-                <button
-                    className='button__text'
-                    onClick={this.toggleCompleted}
-                >
-                    {this.state.showCompleted ? 'Hide Completed (15)' : 'Show Completed (15)'}
-                </button>
-            </section>
-        )
-    }
+export default function ContractList ({sortedContracts, changeSort, toggleCompleted, sort, showCompleted}) {
+        
+    return (
+        <section className={styles.contractList}>
+            <ul>
+                <li className={styles.header}>
+                    <div className={styles.left}>
+                        <button
+                            className={`${styles.col__name} ${styles.th}`}
+                            onClick={(e) => changeSort('nameDesc', e)}
+                        >
+                            <h6>name</h6>
+                            {
+                                /* conditionally render arrow */
+                                sort === 'nameDesc' ||
+                                sort === 'nameAsc'
+                                ? <MdArrowDownward size='1rem' className={`${styles.sortArrow} ${sort === 'nameAsc' ? 'flipped' : null}`}/>
+                                : null
+                            }
+                        </button>
+                        <button
+                            className={`${styles.col__starting} ${styles.th}`}
+                            onClick={(e) => changeSort('dateDesc', e)}
+                        >
+                            <h6>starting</h6>
+                            {
+                                /* conditionally render arrow */
+                                sort === 'dateDesc' ||
+                                sort === 'dateAsc'
+                                ? <MdArrowDownward size='1rem' className={`${styles.sortArrow} ${sort === 'dateAsc' ? 'flipped' : null}`}/>
+                                : null
+                            }
+                        </button>
+                        <button
+                            className={`${styles.col__business} ${styles.th}`}
+                            onClick={(e) => changeSort('busDesc', e)}
+                        >
+                            <h6>business</h6>
+                            {
+                                /* conditionally render arrow */
+                                sort === 'busDesc' ||
+                                sort === 'busAsc'
+                                ? <MdArrowDownward size='1rem' className={`${styles.sortArrow} ${sort === 'busAsc' ? 'flipped' : null}`}/>
+                                : null
+                            }
+                        </button>
+                        <button
+                            className={`${styles.col__quantity} ${styles.th}`}
+                            onClick={(e) => changeSort('quantityDesc', e)}
+                        >
+                            {
+                                /* conditionally render arrow */
+                                sort === 'quantityDesc' ||
+                                sort === 'quantityAsc'
+                                ? <MdArrowDownward size='1rem' className={`${styles.sortArrow} ${sort === 'quantityAsc' ? 'flipped' : null}`}/>
+                                : null
+                            }
+                            <h6>quantity</h6>
+                        </button>
+                    </div>
+                </li>
+                {sortedContracts.map(contract => 
+                    <Contract 
+                        name={contract.name}
+                        business={contract.business}
+                        quantity={numeral(contract.quantity).format("0,0")}
+                        starting={moment(contract.starting).format("MM/DD/YYYY")}
+                        status={contract.status}
+                        key={contract.id}
+                        id={contract.id}
+                    />
+                )}
+            </ul>
+            <button
+                className='button__text'
+                onClick={toggleCompleted}
+            >
+                {showCompleted ? 'Hide Completed (15)' : 'Show Completed (15)'}
+            </button>
+        </section>
+    )
 }
